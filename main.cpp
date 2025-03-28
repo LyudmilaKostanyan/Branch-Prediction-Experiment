802095214e48a9ea24012afbc4455c642a9db9de
LyudmilaKostanyan <lyudakstnn@gmail.com>
2025-03-28 19:31:40 +0400

    Refactor data generation functions to use integer vectors and integrate zen library for random number generation

--- main.cpp
+++ main.cpp
@@ -3,61 +3,41 @@
 #include <random>
 #include <vector>
 #include <iomanip>
+#include "kaizen.h"
 
 using namespace std;
 using namespace std::chrono;
 
-vector<bool> generateRandomData(size_t size) {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(0, 1);
-    
-    vector<bool> data(size);
-    for (size_t i = 0; i < size; ++i) {
-        data[i] = dis(gen);
-    }
+vector<int> generateRandomData(size_t size) {
+    vector<int> data(size);
+    for (size_t i = 0; i < size; ++i)
+        data[i] = zen::random_int(-10000000, 10000000);
     return data;
 }
 
-vector<bool> generatePredictableData(size_t size) {
-    vector<bool> data(size);
-    for (size_t i = 0; i < size; ++i) {
-        data[i] = (i % 2 == 0);
-    }
+vector<int> generatePredictableData(size_t size) {
+    vector<int> data(size);
+    for (size_t i = 0; i < size; ++i)
+        data[i] = i;
     return data;
 }
 
-nanoseconds measureBranchPerformance(const vector<bool>& data, int iterations, volatile int& sum) {
-    auto start = high_resolution_clock::now();
+auto measureBranchPerformance(const vector<int>& data, int iterations, volatile int& sum) {
+    zen::timer timer;
+    timer.start();
     for (int iter = 0; iter < iterations; ++iter) {
         for (size_t i = 0; i < data.size(); ++i) {
-            if (data[i]) {
+            if (data[i] < 0)
                 sum += 1;
-            } else {
+            else
                 sum += 2;
-            }
         }
     }
-    auto end = high_resolution_clock::now();
-    return duration_cast<nanoseconds>(end - start);
+    timer.stop();
+    return timer.duration<zen::timer::msec>();
 }
 
-int main() {
-    const size_t SIZE = 10000000;
-    const int ITERATIONS = 100;
-
-    vector<bool> randomData = generateRandomData(SIZE);
-    vector<bool> predictableData = generatePredictableData(SIZE);
-    
-    volatile int randomSum = 0;
-    volatile int predictableSum = 0;
-    
-    auto randomDuration = measureBranchPerformance(randomData, ITERATIONS, randomSum);
-    auto predictableDuration = measureBranchPerformance(predictableData, ITERATIONS, predictableSum);
-    
-    double randomMs = randomDuration.count() / 1000000.0;
-    double predictableMs = predictableDuration.count() / 1000000.0;
-    
+void printResults(double randomMs, double predictableMs) {
     double speedup = randomMs / predictableMs;
     double difference = randomMs - predictableMs;
     
@@ -76,6 +56,25 @@ int main() {
          << setw(15) << setprecision(2) << speedup
          << difference << endl;
     cout << setfill('-') << setw(85) << "" << endl;
+}
+
+int main() {
+    const size_t SIZE = 10000000;
+    const int ITERATIONS = 100;
+
+    vector<int> randomData = generateRandomData(SIZE);
+    vector<int> predictableData = generatePredictableData(SIZE);
+    
+    volatile int randomSum = 0;
+    volatile int predictableSum = 0;
+    
+    auto randomDuration = measureBranchPerformance(randomData, ITERATIONS, randomSum);
+    auto predictableDuration = measureBranchPerformance(predictableData, ITERATIONS, predictableSum);
+    
+    double randomMs = randomDuration.count();
+    double predictableMs = predictableDuration.count();
+    
+    printResults(randomMs, predictableMs);
 
     return 0;
 }
